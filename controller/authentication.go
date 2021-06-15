f7958e90dc9e56240a3c93c81ed6ea059703d02a
D14 <techcom.ec20@gmail.com>
2021-06-15 16:37:00 +0700

    update: authentication

--- controller/authentication.go
+++ controller/authentication.go
@@ -1,32 +1,21 @@
 package controller
 
 import (
-	"consolidated/entity"
 	"consolidated/helper"
+	"consolidated/model"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Login(c *gin.Context)  {
-	// custom := new(entity.Customer)
-	custom := []entity.User{}
-	// err := service.FindAll(&custom)
-	// if err != nil {
-	// 	helpers.RespondJSON(c, 404, err.Error(), custom)
-	// } else {
-	// 	helpers.RespondJSON(c, 200, "success", custom)
-	// }
-
-	helper.RespondJSON(c, 200, "success", custom)
-
-	// var credentials model.Credentials
-	// err := ctx.ShouldBind(&credentials)
-	// if err != nil {
-	// 	return ""
-	// }
-	// isAuthenticated := service.Login(credentials.Username, credentials.Password)
-	// if isAuthenticated {
-	// 	return service.GenerateToken(credentials.Username, true)
-	// }
-	// return ""
+func Login(ctx *gin.Context) {
+	var login model.Login
+	err := ctx.ShouldBind(&login)
+	if err != nil {
+		helper.RespondJSON(ctx, http.StatusUnauthorized,
+			"username or password is not authenticated",
+			helper.GetInvalidMessage(err))
+	} else {
+		helper.RespondJSON(ctx, http.StatusOK, "login ok", nil)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	gorm.io/driver/sqlserver v1.0.7
 	gorm.io/gorm v1.21.10 // indirect
 )
