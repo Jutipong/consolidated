9fcd7e586ed9063bc72c7b3ec97abf689fe0b6c5
D14 <techcom.ec20@gmail.com>
2021-06-16 17:59:03 +0700

    update: repository  add defer rows.Close()

--- repository/user.go
+++ repository/user.go
@@ -6,32 +6,46 @@ import (
 )
 
 func FindAll(u *[]entity.User) (err error) {
+	rows, err := config.DB.Table("User").Find(&u).Rows()
 	if err = config.DB.Table("User").Find(&u).Error; err != nil {
 		return err
 	}
+	defer rows.Close()
 	return nil
 }
 
 func FindID(u *entity.User, id string) (err error) {
-	if err = config.DB.Table("User").Where("id = ?", id).First(u).Error; err != nil {
+	rows, err := config.DB.Table("User").Where("id = ?", id).First(u).Rows()
+	if err != nil {
 		return err
 	}
+	defer rows.Close()
 	return nil
 }
 
 func AddNewCustomer(u *entity.User) (err error) {
-	if err = config.DB.Table("User").Create(u).Error; err != nil {
+	rows, err := config.DB.Table("User").Create(u).Rows()
+	if err != nil {
 		return err
 	}
+	defer rows.Close()
 	return nil
 }
 
 func PutOneCustomer(u *entity.User, id string) (err error) {
-	config.DB.Table("User").Where("id = ?", id).Save(u)
+	rows, err := config.DB.Table("User").Where("id = ?", id).Save(u).Rows()
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 	return nil
 }
 
 func DeleteCustomer(u *entity.User, id string) (err error) {
-	config.DB.Table("User").Where("id = ?", id).Delete(u)
+	rows, err := config.DB.Table("User").Where("id = ?", id).Delete(u).Rows()
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 	return nil
 }
