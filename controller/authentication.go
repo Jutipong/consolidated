a2e59dff9acc9ffec58906c93d4b8195d7ad86bf
D14 <techcom.ec20@gmail.com>
2021-06-28 12:48:15 +0700

    update: authentication

--- controller/authentication.go
+++ controller/authentication.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"consolidated/helper"
 	"consolidated/model"
+	"encoding/base64"
+	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,8 +15,7 @@ type AuthenticatedController struct{}
 
 //## No Get Logger File
 func (auth *AuthenticatedController) Login(c *gin.Context) {
-	var login model.Login
-	err := c.ShouldBind(&login)
+	login, err := basicAuth(c)
 	if err != nil {
 		helper.JsonResult(c, http.StatusUnauthorized,
 			"username or password is not authenticated",
@@ -22,4 +24,22 @@ func (auth *AuthenticatedController) Login(c *gin.Context) {
 		token := helper.JwtGenerate(login)
 		c.JSON(http.StatusOK, gin.H{"token": token})
 	}
+}
+
+func basicAuth(c *gin.Context) (userLogin model.Login, err error) {
+	auth := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
+	if len(auth) != 2 || auth[0] != "Basic" {
+		err := errors.New("Unauthorized")
+		return userLogin, err
+	}
+
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
+
+	if len(pair) != 2 {
+		return userLogin, errors.New("Unauthorized")
+	}
+	userLogin.Username = pair[0]
+	userLogin.Password = pair[1]
+	return userLogin, nil
 }
