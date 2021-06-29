fcadb13fc1c481cb59126d33d64fbde2bf3d033b
D14 <techcom.ec20@gmail.com>
2021-06-29 14:58:59 +0700

    update: validate message

--- utils/validate.go
+++ utils/validate.go
@@ -2,9 +2,33 @@ package utils
 
 import "github.com/gookit/validate"
 
+// type ErrosMsg struct {
+// 	Field       string
+// 	Description []map[string]string
+// }
+
+// func GetValidateError(v *validate.Validation) interface{} {
+// 	errs := []ErrosMsg{}
+
+// 	for fieldName, validations := range v.Errors {
+// 		err := ErrosMsg{
+// 			Field:       fieldName,
+// 			Description: make([]map[string]string, 0),
+// 		}
+
+// 		for validateType, errMessage := range validations {
+// 			des := map[string]string{}
+// 			des[validateType] = errMessage
+// 			err.Description = append(err.Description, des)
+// 		}
+// 		errs = append(errs, err)
+// 	}
+// 	return errs
+// }
+
 type ErrosMsg struct {
 	Field       string
-	Description []map[string]string
+	Description []string
 }
 
 func GetValidateError(v *validate.Validation) interface{} {
@@ -13,13 +37,11 @@ func GetValidateError(v *validate.Validation) interface{} {
 	for fieldName, validations := range v.Errors {
 		err := ErrosMsg{
 			Field:       fieldName,
-			Description: make([]map[string]string, 0),
+			Description: make([]string, 0),
 		}
 
-		for validateType, errMessage := range validations {
-			des := map[string]string{}
-			des[validateType] = errMessage
-			err.Description = append(err.Description, des)
+		for _, errMessage := range validations {
+			err.Description = append(err.Description, errMessage)
 		}
 		errs = append(errs, err)
 	}
